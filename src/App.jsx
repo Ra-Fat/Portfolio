0a985d09c124b6fe5645942b7f0770a1a80cacfe
RaFat <manrafat2006@gmail.com>
2025-10-13 20:55:57 +0700

    clear the loader

--- src/App.jsx
+++ src/App.jsx
@@ -10,42 +10,16 @@ import Projects from './components/Projects';
 import Skills from './components/Skill';
 import Experiences from './components/Experiences';
 import StarsCanvas from './canvas/Stars';
-import LoaderBounce from './components/AnimateBounce';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [animateLoader, setAnimateLoader] = useState(false);
 
   useEffect(() => {
     AOS.init({
       duration: 1000,
       once: true,
     });
-
-    // Start fade/zoom animation after 3s
-    const timer = setTimeout(() => {
-      setAnimateLoader(true);
-
-      // Remove loader after animation completes
-      setTimeout(() => {
-        setLoading(false);
-      }, 800); // match animation duration
-    }, 3000);
-
-    return () => clearTimeout(timer);
   }, []);
 
-  if (loading) {
-    return (
-      <div
-        className={`fixed inset-0 flex justify-center items-center bg-black z-50 transition-all duration-800 ${
-          animateLoader ? 'opacity-0 scale-90' : 'opacity-100 scale-100'
-        }`}
-      >
-        <LoaderBounce />
-      </div>
-    );
-  }
 
   return (
     <div className="relative bg-gradient-to-br from-gray-900 via-black to-gray-900 text-white min-h-screen flex flex-col gap-10 sm:gap-15 px-5 py-5 sm:px-7 sm:py-7">
